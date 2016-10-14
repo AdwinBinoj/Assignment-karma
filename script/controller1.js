2d70b685a665ae970a8abf86d93a9702e079f8ab
unknown <548745@PC269898.cts.com>
2016-10-14 13:53:29 +0530

    assign

---
+++ script/controller1.js
@@ -0,0 +1,31 @@
+App.controller("MyCtrl1",['$scope','$rootScope','$uibModal','$translatePartialLoader','$translate','fact',function ($scope,$rootScope,$uibModal,$translatePartialLoader,$translate,fact){
+   $translatePartialLoader.addPart('language');
+   $translate.refresh();
+   $rootScope.language="English";
+   $translate.use($rootScope.language); 
+$rootScope.task=[{taskname:"Task1",
+              taskid:"101"},
+              {taskname:"Task2",
+              taskid:"102"}]
+              fact.passdata($rootScope.task);
+ $scope.addoption=function(){
+ var modalInstance = $uibModal.open({
+      controller: 'MyCtrl2',
+      templateUrl: 'popup.html',
+                });
+ }
+ $scope.remove=function(enteredid){
+    for (var i = 0; i < $rootScope.task.length; i++) {
+      if($rootScope.task[i].taskid == enteredid){
+        $rootScope.task.splice(i , 1);
+          fact.passdata($rootscope.task);
+      }
+    }
+
+}
+
+$scope.change=function(lan){
+       $translate.use(lan);
+       $rootScope.language=lan; 
+ }
+}]);//controller ends
